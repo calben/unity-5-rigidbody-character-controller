a382a47a43750bfbb24fed926f5b4262d2ecb402
Calem Bendell <calem.j.bendell@gmail.com>
2015-03-08 01:01:57 -0500

    adding initial material

---
+++ Assets/Scripts/Thrusters.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Thrusters : MonoBehaviour {
+
+  public float thrusterPower;
+
+  [RPC]
+  public void Activate()
+  {
+    this.gameObject.GetComponent<Rigidbody>().AddForce(this.gameObject.transform.forward * this.gameObject.GetComponent<Rigidbody>().mass * thrusterPower, ForceMode.Impulse);
+  }
+
+}
